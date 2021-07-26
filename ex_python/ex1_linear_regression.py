3d7fea5e71e264d071cf67a69445eff85ba7ad62
Ojelle M. Rogero <ojelle.rogero@gmail.com>
2021-07-27 05:55:19 +0800

    Add files via upload
    ex1 linear regression in python done

--- ex_python/ex1_linear_regression.py
+++ ex_python/ex1_linear_regression.py
@@ -1,37 +1,125 @@
+"""
+    Author: Ojelle Rogero
+    Date Created: July 21, 2021
+    Date Finished: July 27, 2021
+    Definition: Linear Regression exercise from Andrew Ng's MAachine Learning course. Octave/Matlab script replicated using Python
+"""
+
 import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
-
-class linear_regression():
-    """
-       Author: Ojelle Rogero
-       Date: July 21, 2021
-       Definition: Linear Regression exercise from Andrew Ng's MAachine Learning course. Octave/Matlab script replicated using Python
-    """
-    
+class Data():
+    """Read txt data and create dataframe"""
     
     # Define properties/attributes
     def __init__(self):
-        self.data = r'C:\Users\ojell\Desktop\Oj\3_Coursera\ML\exercises\ex1_linreg\ex1data1.txt'
+        self.X_data = self.readData().loc[:, 'Profit']
+        self.y_data = self.readData().loc[:, 'City Population']
     
-    # Define data file
-    def read_data(self):
-        df = pd.read_csv(self.data, header=None, names=['Profit', 'City Population'])
-        X = df.iloc[:, 0]
-        y = df.iloc[:, 1]
-        theta = [0, 0]   # initialize theta value
-        
-        return print(len(X))
+    # Define read data method
+    def readData(self):
+        path = r'C:\Users\ojell\Desktop\Oj\3_Coursera\ML\exercises\ex1_linreg'
+        df = pd.read_csv(path + r'\ex1data1.txt', header=None, names=['Profit', 'City Population'])
+        return df    
     
-    # Define cost function
-    def cost_function(self):
-        J = 0
-        for i in range(0, len(X)):
-            J = J  # get back here
-            
-        pass
+class PlotData():
     
+    # Define properties/attributes
+    def __init__(self):
+        input_data = Data()
+        self.X = input_data.X_data
+        self.y = input_data.y_data
     
-    # Define gradient descent
+    # Define plot the data method
+    def plot(self):
+        plt.plot(self.X, self.y, 'rx')
+        plt.xlabel('City Population in 10,000')
+        plt.ylabel('Profit in $10,000s')
+        plt.title('City Population vs Profit')
+        return plt
+    
+    # Define plot the hypothesis method
+    def plotHyp(self, theta_up):
+        y = theta_up[0] + theta_up[1] * self.X
+        plt = self.plot()
+        plt.plot(self.X, y, '-b')
+        plt.legend(['Training Data', 'Linear Regression'])
+        return plt
+    
 
-test = linear_regression()
-test.read_data()
+class linear_regression():    
+    """Linear Regression with 1 Feature"""
+    
+    # Define properties/attributes
+    def __init__(self):
+        input_data = Data()
+        self.X = input_data.X_data
+        self.y = input_data.y_data
+        self.m = len(self.X)
+    
+    # Define cost function method
+    def costFunction(self, theta):
+        m = self.m
+        temp = 0
+        for i in range(0, m):
+            temp += pow((theta[0] + theta[1] * self.X[i]) - self.y[i], 2)
+        
+        J = temp / (2 * m)
+        return J
+    
+    # Define update theta method
+    def thetaUpdate(self, theta):
+        m = self.m
+        theta_up = []
+        tmp0, tmp1, temp0, temp1 = 0, 0, 0, 0
+        for i in range(0, m):
+            tmp0 += (theta[0] + theta[1] * self.X[i]) - self.y[i]
+            tmp1 += ((theta[0] + theta[1] * self.X[i]) - self.y[i]) * self.X[i]
+        temp0 = theta[0] - ((alpha / m) * tmp0)
+        temp1 = theta[1] - ((alpha / m) * tmp1)
+        theta_up = [temp0, temp1]
+    
+        return theta_up
+    
+    # Define gradient descent method
+    def gradientDescent(self, theta, alpha, iters):
+        m = len(self.X)
+        for itr in range(0, iters):
+            thetaUp = self.thetaUpdate(theta)            
+            J_up = self.costFunction(thetaUp)
+            theta = thetaUp
+            
+        return J_up, theta
+
+
+if __name__ == "__main__":
+    
+    # Plot the Data
+    pltData = PlotData()
+    plot = pltData.plot()
+    plot.show()
+    
+    # Calculate cost function and Gradient Descent
+    theta = [0, 0]
+    alpha = 0.01
+    iterations = 1500
+    linreg = linear_regression()
+    costF1 = linreg.costFunction(theta)
+    costF2 = linreg.costFunction([-1, 2])
+    min_J = linreg.gradientDescent(theta, alpha, iterations)[0]
+    theta_updated = linreg.gradientDescent(theta, alpha, iterations)[1]
+    print(f"With theta = {theta}\nComputed cost function J = {round(costF1, 2)}\n")
+    print(f"With theta = {[-1, 2]}\nComputed cost function J = {round(costF2, 2)}\n")
+    print(f"The minimized cost function J is equal to {round(min_J, 2)} with theta = {[round(i, 4) for i in theta_updated]}\n")
+
+    # Plot the Data with the model (which is the h = theta_updated[0] + theta_updated[1] * x)
+    plt_h = pltData.plotHyp(theta_updated)
+    plt_h.show()
+    
+    # Predict values using the updated theta values
+    print("Predict values for population sizes of 35,000 and 70,000")
+    predict1 = theta_updated[0] + theta_updated[1] * 3.5
+    predict2 = theta_updated[0] + theta_updated[1] * 7
+    print(f"For population = 35,000, the predicted profit is {round(predict1*10000, 4)}")
+    print(f"For population = 70,000, the predicted profit is {round(predict2*10000, 4)}")
